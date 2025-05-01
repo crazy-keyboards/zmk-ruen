1f9c5ce7f488ab84788b1cd90377dde0f486f1e1
Iosif Pravets <i@pravets.ru>
2025-05-02 02:56:22 +0300

    Update ruen_win.h
    - исправил опечатку
    
    Signed-off-by: Iosif Pravets <i@pravets.ru>

--- include/zmk/ruen_win.h
+++ include/zmk/ruen_win.h
@@ -1,6 +1,6 @@
 #define RUEN_AT &ruen_one_key 1 AT // @
 #define RUEN_HASH &ruen_one_key 1 HASH // #
-#define RUEN_DLR &ruen_one_key 1 DLR // $
+#define RUEN_DLLR &ruen_one_key 1 DLLR // $
 #define RUEN_CARET &ruen_one_key 1 CARET // ^
 #define RUEN_AMPS &ruen_one_key 1 AMPS // &
 #define RUEN_LBKT &ruen_one_key 1 LBKT // [
