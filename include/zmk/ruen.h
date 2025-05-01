5702ea76587833b2802b61ee207196bdfa826a84
Iosif Pravets <chamaileontas@ya.ru>
2025-05-01 23:38:14 +0000

    добавил все клавиши для ruen

--- include/zmk/ruen.h
+++
@@ -1,6 +0,0 @@
-#define RUEN_AT &ruen_one_key 1 AT
-#define RUEN_HASH &ruen_one_key 1 HASH 
-#define RUEN_DLLR &ruen_one_key 1 DLLR
-#define RUEN_CARET &ruen_one_key 1 CARET
-#define RUEN_AMPS &ruen_one_key 1 AMPS
-#define RUEN_STAR &ruen_one_key 1 STAR
