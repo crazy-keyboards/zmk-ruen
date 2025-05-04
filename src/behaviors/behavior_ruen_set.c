e43984ba0b66f18b72c98e22e286db72200265dc
Iosif Pravets <chamaileontas@ya.ru>
2025-05-04 22:01:18 +0000

    тест

--- src/behaviors/behavior_ruen_set.c
+++ src/behaviors/behavior_ruen_set.c
@@ -1,4 +1,4 @@
-#define DT_DRV_COMPAT zmk_behavior_ruen_set
+#define DT_DRV_COMPAT zmk_behavior_ruen_switch
 
 #include <zephyr/device.h>
 #include <drivers/behavior.h>
@@ -9,20 +9,28 @@
 #include <zmk/endpoints.h>
 #include <zephyr/kernel.h>
 
-static int on_ruen_set_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+static int on_ruen_switch_pressed(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+    return ZMK_BEHAVIOR_OPAQUE;
+}
+
+static int on_ruen_switch_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
+    uint8_t wait = zmk_ruen_get_macos() ? 50 : 5;
     bool is_eng = binding->param1 != 0;
+    uint32_t code = binding->param2;
+    zmk_hid_keyboard_clear();
+    zmk_endpoints_send_report(HID_USAGE_KEY);
     zmk_ruen_set_eng(is_eng);
+    raise_zmk_keycode_state_changed_from_encoded(code, true, event.timestamp);
+    k_msleep(5);
+    raise_zmk_keycode_state_changed_from_encoded(code, false, event.timestamp + 5);
+    k_msleep(wait);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static int on_ruen_set_released(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event) {
-    return ZMK_BEHAVIOR_OPAQUE;
-}
-
-static const struct behavior_driver_api behavior_ruen_set_driver_api = {
-    .binding_pressed = on_ruen_set_pressed,
-    .binding_released = on_ruen_set_released,
+static const struct behavior_driver_api behavior_ruen_switch_driver_api = {
+    .binding_pressed = on_ruen_switch_pressed,
+    .binding_released = on_ruen_switch_released,
 };
 
-#define RUEN_SET_INST(n) BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, NULL, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ruen_set_driver_api)
-DT_INST_FOREACH_STATUS_OKAY(RUEN_SET_INST)
+#define RUEN_SWITCH_INST(n) BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, NULL, NULL, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_ruen_switch_driver_api)
+DT_INST_FOREACH_STATUS_OKAY(RUEN_SWITCH_INST)
